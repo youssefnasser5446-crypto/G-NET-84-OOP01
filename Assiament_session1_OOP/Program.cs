ad7e956d4ea4932adc586451f14321467992b2cf
youssefnasser5446-crypto <youssef.nasser5446@gmail.com>
2026-07-31 03:49:52 +0300

    Question01

--- Assiament_session1_OOP/Program.cs
+++ Assiament_session1_OOP/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question01 
+
+            /*
+             Consider the following code:
+
+            public struct DeliveryAddress
+                    {
+                         public string City; public string Street;
+                    }
+
+            public class Customer
+                 {
+                      public string Name;
+                 }
+
+             a) What happens when a DeliveryAddress variable 
+            is copied into another variable and the copy is modified?
+
+            ans =>DeliveryAddress(value type) It will not affect the original object, and its values ​​will not change.
+
+            b)What happens when a Customer variable is copied into another variable
+            and one variable modifies the object
+
+            ans =>Customer(reference type) The values ​​of both objects will change to the new value 
+            because they are aliases for the same object on the heap.
+             */
+
+            #endregion
+
+
+
         }
     }
 }
