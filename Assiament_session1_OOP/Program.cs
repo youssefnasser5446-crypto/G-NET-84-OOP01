f6c598957c1d6f4e498f09887a5970d1bc786210
youssefnasser5446-crypto <youssef.nasser5446@gmail.com>
2026-07-31 03:33:04 +0300

    Add project files.

---
+++ Assiament_session1_OOP/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assiament_session1_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
